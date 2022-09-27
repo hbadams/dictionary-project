941693e477df0e973d7a7263f0e4e78faae64637
hbadams <channell350@gmail.com>
2022-09-27 16:22:04 -0400

    Styled the application by component, added some behaviours

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,22 +1,17 @@
 import React from "react";
+import "./Meaning.css";
 import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
-    <div>
-      <h3 className="Meaning">{props.meaning.partOfSpeech}</h3>
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition: </strong>
-              {definition.definition}
-              <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={props.meaning.synonyms} />
-            </p>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={props.meaning.synonyms} />
           </div>
         );
       })}
